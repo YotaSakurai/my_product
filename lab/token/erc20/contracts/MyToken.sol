11594cc43a0697bbaa66db4d5b2f4d582906654e
Sakurai <sakuyouyai@gmail.com>
2020-07-21 14:25:15 +0900

    とりあえずコミットしといた

--- lab/token/erc20/contracts/MyToken.sol
+++ lab/token/erc20/contracts/MyToken.sol
@@ -1,15 +1,21 @@
-pragma solidity ^0.6.2;
+//pragma solidity ^0.6.2;
+pragma solidity >=0.5.0 <0.7.0;
 import "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "node_modules/@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 contract MyToken is ERC20{
-    string public name = "MyToken";
-    string public symbol = "MTKN";
-    uint public decimals = 18;
+    string public constant CoinName = "first test coin";
+    string public constant Symbol = "FTC";
+    //初期値100億トークン
+    uint256 constant _initial_supply = 10000000000;
+    address private _MakerAddress;
 
-    function MyToken(uint initialSupply) public{
-        //_totalSupply = initialSupply;
-        //balanceOf[msg.sender] = initialSupply;
-        _mint(msg.sender, initialSupply);
+    constructor() public ERC20(CoinName, Symbol) {
+        _MakerAddress = msg.sender;
+        _mint(msg.sender, _initial_supply);
+    }
+
+    function getMakerAddress() public view returns (address) {
+        return _MakerAddress;
     }
 }
