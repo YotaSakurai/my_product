2de71137e640ede53756c34fba056353413e86fa
Yota Sakurai <sakuyouyai@gmail.com>
2020-08-24 19:15:43 +0900

    resolve migrate error by modified 2_deploy_my_token.js

--- lab/token/erc20/MyToken.sol
+++ lab/token/erc20/MyToken.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.18;
-import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+pragma solidity ^0.5.0;
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is StandardToken{
+contract MyToken is ERC20{
     string public name = "MyToken";
-    string public symbol = "MTKN";
+    string public symbol = "TST";
     uint public decimals = 18;
 
     function MyToken(uint initialSupply) public{
