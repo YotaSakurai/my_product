b5a1362a43d23ec1e9597c20f6d6d658f82660c7
Yota Sakurai <sakuyouyai@gmail.com>
2020-10-01 11:42:26 +0900

    migration

--- lab/token/erc20/migrations/2_deplpy_my_token.js
+++ lab/token/erc20/migrations/2_deplpy_my_token.js
@@ -3,7 +3,7 @@ const MyToken = artifacts.require('./MyToken.sol')
 module.exports = (deployer) => {
     const initialSupply = 50000e18
 
-    //migrate時にエラーが起きる
+    //パラメーターを渡すとmigrate時にエラーが起きる
     //deployer.deploy(MyToken, initialSupply)
     
     deployer.deploy(MyToken)
