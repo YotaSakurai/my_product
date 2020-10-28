82752e7dc7922d5a561d341d1a3ee8238f870d90
Yota Sakurai <sakuyouyai@gmail.com>
2020-10-28 18:34:26 +0900

    tx-create.jsのメモ

---
+++ lab/token/saku/evacoin/lambda/tx-create.js
@@ -0,0 +1,49 @@
+/*-----------------------------
+送信データ：txhash, timestamp
+timestampは日本時間(ISO8601)
+
+
+------------------------------------*/
+
+var AWS = require("aws-sdk");
+var dynamo = new AWS.DynamoDB.DocumentClient();
+var tableName = "users";                 //DynamoDB
+
+exports.handler = (event, context, callback) => {
+    var response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify({"message" : ""})
+    };
+
+    var body = JSON.parse(event.body);
+
+    //TODO: DBに登録するための情報をparamオブジェクトとして宣言する（中身を記述）
+    var param = {
+        "TableName":tableName,
+        "Item":{
+            "id":body.id,
+            "address":body.address,
+            "name":body.name,
+            "totalReceive":body.totalReceive
+        }
+    };
+
+    //dynamo.put()でDBにデータを登録
+    dynamo.put(param, function(err, data) {
+        if (err) {
+            //TODO: 登録に失敗した場合の処理を記述
+            response.statusCode = 500;
+            response.body = JSON.stringify({"message":"error"});
+            callback(null,response);
+            return;
+        } else {
+            //TODO: 登録に成功した場合の処理を記述
+            response.body = JSON.stringify(param.Item);
+            callback(null,response);
+            return;
+        }
+    });
+};
